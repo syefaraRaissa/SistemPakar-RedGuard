e2cb0388f06b9ae014457ba0192d65420e035551
Syefara Raissa Ramadhan <160453549+syefaraRaissa@users.noreply.github.com>
2025-04-27 12:42:25 +0700

    Update sistempakar.py
    Signed-off-by: Syefara Raissa Ramadhan <160453549+syefaraRaissa@users.noreply.github.com>

--- sistempakar.py
+++ sistempakar.py
@@ -14,7 +14,7 @@ st.markdown("""
         display: block;
         margin-left: auto;
         margin-right: auto;
-        width: 50%;
+        width: 20%;
         height: auto;
     }
     .card {
